78b22f60bd38c0132ea602dafde2ab737d5b2350
YaArtem333 <aermilov2003@gmail.com>
2023-10-22 16:25:51 +0300

    Database changes

--- Data.cs
+++ Data.cs
@@ -1,70 +1,74 @@
+using System;
 using System.Text.Json;
 using System.IO;
-namespace sea_battle;
-public class Data
+using System.Collections.Generic;
+
+
+namespace sea_battle
 {
-    private string DbPath { get; set; } = "";
-    
-    public Data(string dbPath = "records.json")
+    public class Data
     {
-        DbPath = dbPath;
-    }
-    public List<PlayerData> GetRecords()
-    {
-        if (File.Exists(DbPath))
+        private string DbPath { get; set; } = "";
+        
+        public Data(string dbPath = "records.json")
         {
+            DbPath = dbPath;
+        }
+
+        public void AddRecord(PlayerData playerData)
+        {
+            List<PlayerData> records = GetRecords();
+            records.Add(playerData);
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true  // Включает форматирование с отступами
+            };
+            string json = JsonSerializer.Serialize(records, options);
+            File.WriteAllText(DbPath, json);
+        }
+
+        private List<PlayerData> GetRecords()
+        {
+            if (!File.Exists(DbPath)) return new List<PlayerData>();
+
             string json = File.ReadAllText(DbPath);
-            return JsonSerializer.Deserialize<List<PlayerData>>(json) ?? new List<PlayerData>();
+
+            try
+            {
+                return JsonSerializer.Deserialize<List<PlayerData>>(json);
+            }
+            catch (JsonException)
+            {
+                return new List<PlayerData>();
+            }
         }
-        else
+
+        public List<PlayerData> GetRecordsByName(string playerName)
         {
-            return new List<PlayerData>();
+            // Получить все записи
+            List<PlayerData> records = GetRecords();
+
+            // Использовать LINQ для фильтрации записей по имени пользователя
+            List<PlayerData> filteredRecords = records.Where(record => record.Name == playerName).ToList();
+
+            return filteredRecords;
         }
     }
 
-    
-
-    public void CreateOrUpdateRecords(string name, int score, int deadShips)
+    public class PlayerData
     {
-        var users = GetRecords();
-        var user = users.Find(usr => usr.Name == name);
-        if (user == null)
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Result { get; set; }
+
+        public PlayerData() { }
+        public PlayerData(string name, DateTime dateTime, string result)
         {
-            users.Add(new PlayerData(name, score, deadShips));
+            Name = name;
+            Date = dateTime.ToString("yyyy-MM-dd");
+            Time = dateTime.ToString("HH:mm");
+            Result = result;
         }
-        else
-        {
-            user.Score = score;
-            user.DeadShips = deadShips;
-        }
-
-        SaveRecords(users);
     }
-    
-    
-    
-    
-    
-    
-    public void SaveRecords(List<PlayerData> records)
-    {
-        string json = JsonSerializer.Serialize(records);
-        File.WriteAllText(DbPath, json);
-    }
-   
-}
-
-public class PlayerData
-{
-    public string Name { get; set; }
-    public int Score { get; set; }
-    public int DeadShips { get; set; }
-
-    public PlayerData(string name, int score, int deadShips)
-    {
-        Name = name;
-        Score = score;
-        DeadShips = deadShips;
-    }
-
 }
